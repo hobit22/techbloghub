7c0409c4bdc90281654d79dea418906176bb44e8
hobit22 <hobit22@naver.com>
2025-09-11 03:12:05 +0900

    feat: 백엔드 쿼리 수정

--- backend/persistence/src/main/java/com/techbloghub/persistance/repository/PostRepository.java
+++ backend/persistence/src/main/java/com/techbloghub/persistance/repository/PostRepository.java
@@ -1,9 +1,22 @@
 package com.techbloghub.persistance.repository;
 
 import com.techbloghub.persistance.entity.PostEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity, Long>, PostRepositoryCustom {
+    
+    @EntityGraph("PostEntity.withAllRelations")
+    @Query("SELECT p FROM PostEntity p ORDER BY p.publishedAt DESC")
+    Page<PostEntity> findAllWithRelationsOrderByPublishedAtDesc(Pageable pageable);
+    
+    @EntityGraph("PostEntity.withAllRelations")
+    Optional<PostEntity> findById(Long id);
 }
